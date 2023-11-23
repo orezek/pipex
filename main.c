2aaeeaf6215349e1025cb0d334c5325a74e9791d
O.R <orezek@seznam.cz>
2023-11-23 18:50:33 +0100

    Prototyping: implement basic functionality without error checks and dynamic processing.
    This version works as per instructions, however, everything is hard coded and without any error checks - only proof of understanding of inter process comminication

--- main.c
+++ main.c
@@ -6,10 +6,13 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 15:22:40 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/23 18:38:33 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/23 18:46:42 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+// the commands are harcoded for now and the program will output from the file1 to file2
+// through 1 cat command and 2 wc command
 #include "pipex.h"
 
 int	ft_is_file_valid(const char *filename)
@@ -43,9 +46,9 @@ int	main(int argc, char *argv[])
 	int	pipe_fd[2];
 	int pid;
 // argument check
-	if (argc != 4)
+	if (argc != 5)
 	{
-		ft_putstr_fd("Usage: ./pipex file1 cmd1 file2", 2);
+		ft_putstr_fd("Usage: ./pipex file1 cmd1 cmd2 file2", 2);
 		return (1);
 	}
 // file check of the first argument
@@ -58,7 +61,7 @@ int	main(int argc, char *argv[])
 	}
 // create input and output file descriptors
 	input_fd = open(argv[1], O_RDONLY);
-	output_fd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	output_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (output_fd == -1)
 		return (1);
 // create pipe in parent process
@@ -77,10 +80,11 @@ int	main(int argc, char *argv[])
 
 		if (pipe(pipe_fd2) == -1)
 			return (1);
-		// subchild process execution
+		// create subchild process
 		pid2 = fork();
 		if (pid2 == -1)
 			return (1);
+		// subchild process execution a child of the child process
 		if (pid2 == 0)
 		{
 			// this was crazy! 3 hours of debugging and it was just a close() missing
@@ -99,6 +103,7 @@ int	main(int argc, char *argv[])
 			execve("/usr/bin/wc", wc_args, NULL);
 		}
 		// child process execution
+		// child of the main process
 		else
 		{
 			// close pipe_fd[1] - not needed for writing in the child process to the main process
@@ -120,7 +125,8 @@ int	main(int argc, char *argv[])
 			waitpid(pid2, NULL, 0);
 		}
 	}
-// parent process execution - executes in parallel with child process! (pid == 0) is the child process
+// parent process execution - executes in parallel with child process!
+// (pid == 0) is the child process
 	else
 	{
 		// close pipe_fd[0] - not needed for reading in the parent process
