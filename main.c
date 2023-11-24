ad485a72c8318e5a3b35b9a16a0c2b47dd7b0124
O.R <orezek@seznam.cz>
2023-11-24 18:19:57 +0100

    Prototyping: create pipes dynamimically on no of commands

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 15:22:40 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/24 17:43:39 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/24 18:15:25 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,18 @@ int	main(int argc, char *argv[])
 {
 	int	input_fd;
 	int	output_fd;
-	int	pipe_fd[2];
+	int	no_of_commands;
+	int	pipe_fd[2 * (argc - 3)];
 	int pid;
+
+	no_of_commands = argc - 3;
+// create pipes for each command
+	int i = 0;
+	while (i < no_of_commands)
+	{
+		if (pipe(pipe_fd + (i++ * 2)) == -1)
+			return (1);
+	}
 // argument check
 	if (argc != 5)
 	{
@@ -64,9 +74,7 @@ int	main(int argc, char *argv[])
 	output_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (output_fd == -1)
 		return (1);
-// create pipe in parent process
-	if (pipe(pipe_fd) == -1)
-		return (1);
+
 // create child process
 	pid = fork();
 // check if fork failed
@@ -76,10 +84,6 @@ int	main(int argc, char *argv[])
 	if (pid == 0)
 	{
 		int pid2;
-		int pipe_fd2[2];
-
-		if (pipe(pipe_fd2) == -1)
-			return (1);
 		// create subchild process
 		pid2 = fork();
 		if (pid2 == -1)
@@ -93,10 +97,10 @@ int	main(int argc, char *argv[])
 			// so you must close ALL ALL ALL!!
 			close(pipe_fd[0]);
 			close(pipe_fd[1]);
-			close(pipe_fd2[1]);
-			dup2(pipe_fd2[0], STDIN_FILENO);
+			close(pipe_fd[3]);
+			dup2(pipe_fd[2], STDIN_FILENO);
 			//ft_print_from_fd(pipe_fd2[0]);
-			close(pipe_fd2[0]);
+			close(pipe_fd[2]);
 			dup2(output_fd, STDOUT_FILENO);
 			close(output_fd);
 			char *wc_args[] = { "wc", "-lwc", NULL };
@@ -108,17 +112,17 @@ int	main(int argc, char *argv[])
 		{
 			// close pipe_fd[1] - not needed for writing in the child process to the main process
 			close(pipe_fd[1]);
-			close(pipe_fd2[0]);
+			close(pipe_fd[2]);
 			// connect the pipe from the main process to the stdin of the child process (ls)
 			//ft_print_from_fd(pipe_fd[0]);
 			dup2(pipe_fd[0], STDIN_FILENO);
 			//- no longer needed
 			close(pipe_fd[0]);
 			// connect the pipe from the child process to the write end of the pipe of the subchild process
-			dup2(pipe_fd2[1], STDOUT_FILENO);
+			dup2(pipe_fd[3], STDOUT_FILENO);
 			//ft_putnbr_fd(dup2(output_fd, STDOUT_FILENO), 1);
 			//close(output_fd);
-			close(pipe_fd2[1]);
+			close(pipe_fd[3]);
 			// execute command
 			char *wc_argss[] = { "cat", NULL };
 			execve("/bin/cat", wc_argss, NULL);
@@ -131,6 +135,8 @@ int	main(int argc, char *argv[])
 	{
 		// close pipe_fd[0] - not needed for reading in the parent process
 		close(pipe_fd[0]);
+		close(pipe_fd[2]);
+		close(pipe_fd[3]);
 		// initialize buffer
         char buffer[1];
 		// initialize bytes_read
