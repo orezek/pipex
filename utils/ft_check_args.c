1bc5d844f1b67d8b63e722437846f785a3c4ad51
O.R <orezek@seznam.cz>
2023-11-30 00:33:01 +0100

    Fix: return perror when no arguments are passed

--- utils/ft_check_args.c
+++ utils/ft_check_args.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 14:36:44 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/30 00:00:50 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/30 00:20:27 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_check_args(int argc, char **argv, char **envp)
 
 	i = 2;
 	if (argc == 1)
-		return (1);
+		return (perror(argv[0]), 1);
 	if ((ft_is_heredoc(argv) && argc < 6)
 		|| (ft_is_file_valid(argv[1]) && argc < 5))
 		return (perror("not enough arguments"), 1);
