19462ede81340d7e944c276fcf86607b6c8719b0
Oldrich Rezek <orezek@seznam.cz>
2023-11-29 11:58:55 +0100

    Update: add helper function signatures

--- pipex.h
+++ pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
+/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 15:23:54 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/22 19:06:05 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/29 11:43:15 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,22 @@
 # define PIPEX_H
 
 # include <fcntl.h>
+# include <sys/wait.h>
 # include <unistd.h>
 # include "./libft/libft.h"
+
+int		ft_is_file_valid(const char *filename);
+int		ft_pip_strncmp(const char *s1, const char *s2, size_t size);
+char	**ft_get_paths(char *envp[]);
+char	*ft_get_command_path(char *envp[], char *cmd_arg);
+int		ft_exec_cmd(char *envp[], char *cmd_arg);
+char	*ft_sanitize_line(char *str);
+void	ft_read_heredoc(char *limiter);
+int		ft_create_io_fd(char *argv[], int argc, int *input_fd, int *output_fd);
+void	ft_cls_fd(int *pipe_fd, int input_fd, int output_fd, int no_of_cmds);
+int		*ft_create_pipes(int no_of_cmds);
+void	ft_redir_pipes(int input_fd, int output_fd, int process, int *pipe_fd, int no_of_cmds);
+int		ft_is_heredoc(char *argv[]);
+int		ft_process(int *process, int no_of_cmds);
+int		ft_check_args(int argc, char **argv, char **envp);
 #endif
