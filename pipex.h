d24253bd709f940ad0fb12d35ab8b4022d06dee3
O.R <orezek@seznam.cz>
2023-11-29 20:08:34 +0100

    Refactor: refactoring ft_create_io_fd()

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 15:23:54 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/29 19:56:16 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/29 20:00:36 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	*ft_get_command_path(char *envp[], char *cmd_arg);
 int		ft_exec_cmd(char *envp[], char *cmd_arg);
 char	*ft_sanitize_line(char *str);
 void	ft_read_heredoc(char *limiter);
-int		ft_create_io_fd(char *argv[], int argc, int *input_fd, int *output_fd);
+int		ft_create_io_fd(char *argv[], int argc, int **io_fd);
 void	ft_cls_fd(int *pipe_fd, int *io_fd, int no_of_commands);
 int		*ft_create_pipes(int no_of_cmds);
 void	ft_redir_pipes(int *io_fd, int process, int *pipe_fd, int no_of_cmds);
--- utils/ft_create_io_fd.c
+++ utils/ft_create_io_fd.c
@@ -6,27 +6,28 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 15:48:08 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/29 15:49:08 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/29 19:59:47 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-int	ft_create_io_fd(char *argv[], int argc, int *input_fd, int *output_fd)
+int	ft_create_io_fd(char *argv[], int argc, int **io_fd)
 {
+	*io_fd = malloc(2 *  sizeof(int));
 	if (ft_pip_strncmp(argv[1], "here_doc", ft_strlen(argv[1])) == 0)
 	{
 		ft_read_heredoc(argv[2]);
-		*input_fd = open(argv[1], O_RDONLY);
-		*output_fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
-		if (*output_fd == -1 || *input_fd == -1)
+		(*io_fd)[0] = open(argv[1], O_RDONLY);
+		(*io_fd)[1] = open(argv[argc - 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+		if ((*io_fd)[1] == -1 || (*io_fd)[0] == -1)
 			return (perror("heredoc file open"), -1);
 	}
 	else
 	{
-		*input_fd = open(argv[1], O_RDONLY);
-		*output_fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (*output_fd == -1 || *input_fd == -1)
+		(*io_fd)[0] = open(argv[1], O_RDONLY);
+		(*io_fd)[1] = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if ((*io_fd)[1] == -1 || (*io_fd)[0] == -1)
 			return (perror("input file open"), -1);
 	}
 	return (0);
